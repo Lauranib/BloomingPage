a37dabcdc8c5c8d3156846334778fe90f3131c1b
Laura <lani0005@stud.kea.dk>
2024-11-12 14:59:44 +0100

    yesh

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        hvid: "fffbe2",
+        pinkpink: "f9c9bb",
+        salmon: "fab9a5",
+        redred: "b94723",
+        deepblue: "05377c",
+      },
+
+      fontFamily: {
+        afacad: ["Afacad"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
